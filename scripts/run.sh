163b31767d9844352637750a7462faed5ac98b4d
Roy Keyes <psyclone241@gmail.com>
2016-03-01 22:52:46 -0500

    Add feedback for the run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,10 +4,23 @@ source scripts/functions.sh
 
 if [ "$1" == "port" ];
 then
-  check_port
+  LIVE_PORT=`check_port`
+  if [ "$LIVE_PORT" != "" ];
+  then
+    echo $LIVE_PORT
+  else
+    echo "No service running on $PORT"
+  fi
 fi
 
 if [ "$1" == "pid" ];
 then
-  check_pid
+  PID=`check_pid`
+  if [ "$PID" != "" ];
+  then
+    echo $PID
+  else
+    echo "No http-server running"
+  fi
+
 fi
